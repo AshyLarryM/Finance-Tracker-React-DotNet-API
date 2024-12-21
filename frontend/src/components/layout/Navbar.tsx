bf4d2948cb5cd3860d553b6459a5c5630b85a5ee
AshyLarryM <LarryM093@gmail.com>
2024-12-21 15:56:39 -0500

    Login, Logout, Welcome {user?.useName}

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
+import { useAuth } from "../../context/useAuth";
 
 export function Navbar() {
+    const { isLoggedIn, user, logout } = useAuth();
     return (
         <nav className="relative container mx-auto p-6">
             <div className="flex items-center justify-between">
@@ -12,15 +14,28 @@ export function Navbar() {
                         </Link>
                     </div>
                 </div>
-                <div className="hidden lg:flex items-center space-x-6 text-teal-300">
-                    <div className="hover:text-teal-500 duration-200 cursor-pointer font-semibold text-lg mr-6">Login</div>
-                    <a
-                        href="/register"
-                        className="px-8 py-3 font-bold text-white border border-teal-500 bg-teal-500 duration-200 hover:bg-opacity-20 hover:text-teal-500 rounded-full"
-                    >
-                        Signup
-                    </a>
-                </div>
+                {isLoggedIn() ? (
+                    <div className="hidden lg:flex items-center space-x-6 text-teal-300">
+                        <div className="hover:text-teal-500 duration-200 cursor-pointer font-semibold text-lg mr-6">Welcome, {user?.userName}</div>
+                        <a
+                            onClick={logout}
+                            className="px-8 py-3 font-bold text-white border border-teal-500 bg-teal-500 duration-200 hover:bg-opacity-20 hover:text-teal-500 rounded-full"
+                        >
+                            Logout
+                        </a>
+                    </div>
+                ) : (
+                    <div className="hidden lg:flex items-center space-x-6 text-teal-300">
+                        <Link to='/login' className="hover:text-teal-500 duration-200 cursor-pointer font-semibold text-lg mr-6">Login</Link>
+                        <Link
+                            to="/register"
+                            className="px-8 py-3 font-bold text-white border border-teal-500 bg-teal-500 duration-200 hover:bg-opacity-20 hover:text-teal-500 rounded-full"
+                        >
+                            Signup
+                        </Link>
+                    </div>
+                )}
+
             </div>
         </nav>
     );
