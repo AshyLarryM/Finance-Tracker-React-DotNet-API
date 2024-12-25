3c408f8dc7c3a98aef74042f55e48f15498cdc7f
AshyLarryM <LarryM093@gmail.com>
2024-12-25 12:35:14 -0500

    Show portfolio cost.

--- frontend/src/components/portfolio/cardPortfolio/CardPortfolio.tsx
+++ frontend/src/components/portfolio/cardPortfolio/CardPortfolio.tsx
@@ -11,6 +11,7 @@ export function CardPortfolio({ portfolioValue, onPortfolioDelete }: CardPortfol
     return (
         <div className="flex flex-col my-8 bg-slate-800 hover:bg-slate-700 duration-500 w-full p-8 space-y-4 text-center border-2 border-slate-800 rounded-xl shadow-lg md:w-1/3">
             <Link to={`/company/${portfolioValue.symbol}/company-profile`} className="text-xl font-bold text-teal-100">{portfolioValue.symbol}</Link>
+            <p className="text-teal-100">Cost: ${portfolioValue.purchase}</p>
             <DeletePortfolio
                 onPortfolioDelete={onPortfolioDelete}
                 portfolioValue={portfolioValue.symbol}
